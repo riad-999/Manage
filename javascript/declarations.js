369f5c5a942c05116bbfffe66a00735b46a8c6eb
Riad <felihriad5@gmail.com>
2021-08-09 11:42:43 +0200

    first commit

---
+++ javascript/declarations.js
@@ -0,0 +1,95 @@
+function show_close_click(event)
+{
+    const close_btn = document.getElementById("close");
+    const show_btn= document.getElementById("show");
+    const top_nav_bar = document.querySelector(".top-nav-bar");
+    close_btn.classList.toggle("not-visible");
+    show_btn.classList.toggle("not-visible");
+    top_nav_bar.classList.toggle("show-nav-bar");
+}
+class Slid
+{
+    static counter = 0;
+    static max = 3;
+    static startX ;
+    static endX;
+    
+    static swip()
+    {
+        const cards = document.querySelectorAll(".card");
+        cards.forEach(card=>
+        {
+            card.style.transform = `translateX(-${Slid.counter}00%)`;
+        });
+    }
+    static next(event)
+    {
+        if(Slid.counter === Slid.max)
+        {
+            Slid.counter = 0;
+            Slid.swip();
+            const current = document.querySelector(".current");
+            const nextElement = current.parentElement.firstElementChild;
+            current.classList.remove("current");
+            nextElement.classList.add("current");
+        }
+        else
+        {
+            ++Slid.counter;
+            Slid.swip();
+            const current = document
+            .querySelector(".current");
+            const nextElement = current.nextElementSibling;
+            current.classList.remove("current");
+            nextElement.classList.add("current");
+        }
+    }
+    static prev(event)
+    {
+        if(Slid.counter === 0)
+        {
+            Slid.counter = 3;
+            Slid.swip();
+            const current = document
+            .querySelector(".current");
+            const prevtElement = current.parentElement.lastElementChild;
+            current.classList.remove("current");
+            prevtElement.classList.add("current");
+        }
+        else
+        {
+            --Slid.counter;
+            Slid.swip();
+            const current = document
+            .querySelector(".current");
+            const prevtElement = current.previousElementSibling;
+            current.classList.remove("current");
+            prevtElement.classList.add("current");
+        }
+    }
+    static pointerdonw(event)
+    {
+        Slid.startX = event.pageX;
+    }
+    static pointerup(event)
+    {
+        Slid.endX = event.pageX;
+        if(Slid.startX < Slid.endX)
+        {
+            Slid.prev();
+        }
+        else
+        {
+            if(Slid.startX != Slid.endX)
+                Slid.next();
+        }
+    }
+}
+function next(event) 
+{
+    Slid.next();
+}
+function prev(event)
+{
+    Slid.prev();
+}
---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+document.querySelector(".show-close-btns").
+addEventListener("click",show_close_click);
+// document.querySelector(".next").addEventListener("click",next);
+// document.querySelector(".prev").addEventListener("click",prev);
+document.querySelector(".cards-container").
+addEventListener("pointerdown",Slid.pointerdonw);
+document.querySelector(".cards-container").
+addEventListener("pointerup",Slid.pointerup);
